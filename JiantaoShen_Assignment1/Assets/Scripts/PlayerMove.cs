d54303d3fc82af61d3d00a678931483f7788e348
J. Shen <karth-shen@hotmail.com>
2018-02-07 07:12:32 -0800

    Updated Assignment 1

---
+++ JiantaoShen_Assignment1/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour 
+
+{
+	[SerializeField] private float moveSpeed = 10.0f;
+
+	private Rigidbody rbd;
+
+	private void Start ()
+	{
+		this.rbd = this.gameObject.GetComponent<Rigidbody> ();
+	}
+
+	private void FixedUpdate ()
+	{
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (x, 0.0f, y);
+
+		this.rbd.AddForce (movement * moveSpeed);
+	}
+
+}
